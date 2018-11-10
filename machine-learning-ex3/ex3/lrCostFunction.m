8a0f828ca8250c2cb71f07f0058f8bb1b6834014
MarcoAntonioMontez <32492889+MarcoAntonioMontez@users.noreply.github.com>
2018-11-10 22:38:49 +0000

    Completed ex3

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,7 +36,12 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
+h = sigmoid(X*theta);
+Jreg = (lambda/(2*m))*(theta(2:end)'*theta(2:end));
+J = (1/m)*(((-y)'*log(h))-((1-y)'*log(1-h)))+Jreg;
+thetaReg = theta;
+thetaReg(1) = 0;
+grad = (1/m)*X'*(h-y) + (lambda/m)*thetaReg;
 
 
 
